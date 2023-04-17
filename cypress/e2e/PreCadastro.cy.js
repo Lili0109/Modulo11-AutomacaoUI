28d10b4d7341155fde468797479ef5eeb94cee80
Lilianne <liliannejrs@gmail.com>
2023-04-17 20:04:35 -0300

    Atualizado

--- cypress/e2e/PreCadastro.cy.js
+++ cypress/e2e/PreCadastro.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 
-describe('Funcionalidade de Pre Cadastro', () => {
+import { faker } from '@faker-js/faker'
+
+describe('Funcionalidade de Pré Cadastro', () => {
     
 
     beforeEach(() => {
@@ -8,13 +10,15 @@ describe('Funcionalidade de Pre Cadastro', () => {
     });
     
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get ('#reg_email'). type ('lili1235@ebac.com')
+        cy.get ('#reg_email'). type (faker.internet.email ())
         cy.get ('#reg_password'). type ('Teste@teste')
         cy.get (':nth-child(4) > .button').click ()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a'). click ()
-        cy.get('#account_first_name'). type ('Lili')
-        cy.get('#account_last_name'). type ('Jorge')
+        cy.get('#account_first_name'). type (faker.name.firstName ())
+        cy.get('#account_last_name'). type (faker.name.lastName ())
         cy.get('.woocommerce-Button'). click ()
+
+        cy.get ('.woocommerce-message').should ('contain' , 'Detalhes da conta modificados com sucesso')
     });
 });
