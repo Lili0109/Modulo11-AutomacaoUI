a17ae64774d8c42e5457daad5ece281b9c3b748d
Lilianne <liliannejrs@gmail.com>
2023-04-18 09:30:23 -0300

    Atualizado

--- cypress/e2e/PreCadastro.cy.js
+++ cypress/e2e/PreCadastro.cy.js
@@ -10,13 +10,19 @@ describe('Funcionalidade de Pré Cadastro', () => {
     });
     
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get ('#reg_email'). type (faker.internet.email ())
+
+        let nomefaker = faker.name.lastName ()
+        let sobrenomefaker = faker.name.firstName ()
+        let emailfaker = faker.internet.email ()
+
+
+        cy.get ('#reg_email'). type (emailfaker)
         cy.get ('#reg_password'). type ('Teste@teste')
         cy.get (':nth-child(4) > .button').click ()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a'). click ()
-        cy.get('#account_first_name'). type (faker.name.firstName ())
-        cy.get('#account_last_name'). type (faker.name.lastName ())
+        cy.get('#account_first_name'). type (sobrenomefaker)
+        cy.get('#account_last_name'). type (nomefaker)
         cy.get('.woocommerce-Button'). click ()
 
         cy.get ('.woocommerce-message').should ('contain' , 'Detalhes da conta modificados com sucesso')
